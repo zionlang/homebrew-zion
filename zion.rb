af78a47f33d7125193ce5c2301c6f025c7e66716
Will Bradley <wbbradley@github.com>
2020-08-04 20:40:41 -0600

    wip

--- zion.rb
+++ zion.rb
@@ -4,7 +4,7 @@ class Zion < Formula
   tarfile = `/tmp/v0.3.0.tar.gz`
   url "file://#{tarfile}"
   # url "https://github.com/zionlang/zion/archive/master.tar.gz"
-  sha256 "9b95c6a23e755366083e5693f0de07a1a61b3a5f5a0fdff8c7c15905c96eb5c9"
+  sha256 "3203fe63dcbb5ec0ad6ef4dd1f9fd4cf3624a24f8821f85a5be7d60a00c2e487"
   license "MIT License"
   version "0.3.0"
 
